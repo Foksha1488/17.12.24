d459c67a16155a911948f6cb4e75f7483efbf269
Foksha1488 <maksimfoksha97@gmail.com>
2024-12-17 09:33:39 +0200

    Task-2

---
+++ 2 задание 17,12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_задание_17_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.ReadKey();
+        } static void Func()
+        {
+            for (int x = 1; x >= 10; x--)
+                Console.WriteLine(x);
+            }
+            
+            
+    }
+
+}
