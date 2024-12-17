dba3671bfd2686681acd5dd3cb6aefe4c529ae50
Foksha1488 <maksimfoksha97@gmail.com>
2024-12-17 09:34:36 +0200

    Task3

---
+++ 17,12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_12
+{
+    internal class Program
+    {/// <summary>
+     /// Вывести в столбик 5 раз слово «Привет!» (циклы с условием)
+     /// </summary>
+     /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Привет!");
+            }
+            Console.ReadKey();
+        }  
+
+    }
+    
+}
